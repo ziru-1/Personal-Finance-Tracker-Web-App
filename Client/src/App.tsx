8da95d378375d2e5dc475b5b3a5f035ce8241afe
Zed <zedcaoile@gmail.com>
2025-06-26 15:16:32 +0800

    wrap Dashboard component with FinancialRecordsProvider to ensure context access

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,20 +1,27 @@
-import './App.css'
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
-import { Dashboard } from './pages/dashboard';
-import { Auth } from './pages/auth';
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard";
+import { Auth } from "./pages/auth";
+import { FinancialRecordsProvider } from "./contexts/financial-record-context";
 
 function App() {
-
   return (
     <Router>
-      <div className='app-container'>
+      <div className="app-container">
         <Routes>
-          <Route path='/' element={<Dashboard />}/>
-          <Route path='/auth' element={<Auth />}/>
+          <Route
+            path="/"
+            element={
+              <FinancialRecordsProvider>
+                <Dashboard />
+              </FinancialRecordsProvider>
+            }
+          />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </div>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
