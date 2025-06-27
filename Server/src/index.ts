28ee371faf752260a45b7a5fae38143148b8488f
Zed <zedcaoile@gmail.com>
2025-06-27 14:15:27 +0800

    add cors

--- Server/src/index.ts
+++ Server/src/index.ts
@@ -4,11 +4,13 @@ dotenv.config(); // Load environment variables from .env
 import express, {Express} from "express";
 import mongoose from "mongoose";
 import financialRecordRouter from './routes/financial-records';
+import cors from 'cors';
 
 const app: Express = express();
 const port = process.env.PORT || 3001;
 
 app.use(express.json());
+app.use(cors());
 
 const mongoURI: string = process.env.DB_URL!;
 
