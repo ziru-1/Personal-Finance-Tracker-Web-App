a5c70cb9298ad46b19afc9d78d1ac664007d9c1a
Zed <zedcaoile@gmail.com>
2025-06-27 18:45:19 +0800

    add fetchRecords for get request

--- Client/src/contexts/financial-record-context.tsx
+++ Client/src/contexts/financial-record-context.tsx
@@ -1,6 +1,7 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useUser } from "@clerk/clerk-react";
 
-interface FinancialRecord {
+export interface FinancialRecord {
   id?: string;
   userID: string;
   date: Date;
@@ -27,6 +28,25 @@ export const FinancialRecordsProvider = ({
   children: React.ReactNode;
 }) => {
   const [records, setRecords] = useState<FinancialRecord[]>([]);
+  const { user } = useUser();
+
+  const fetchRecords = async () => {
+    if (!user) return;
+    const response = await fetch(
+      `http://localhost:3001/financial-records/getAllByUserID/${user.id}`
+    );
+
+    if (response.ok) {
+      const records = await response.json();
+      console.log(records);
+      setRecords(records);
+    }
+  };
+
+  useEffect(() => {
+    fetchRecords();
+  }, [user]);
+
   const addRecord = async (record: FinancialRecord) => {
     const response = await fetch("http://localhost:3001/financial-records", {
       method: "POST",
@@ -46,7 +66,6 @@ export const FinancialRecordsProvider = ({
 
   return (
     <FinancialRecordsContext.Provider value={{ records, addRecord }}>
-      {""}
       {children}
     </FinancialRecordsContext.Provider>
   );
